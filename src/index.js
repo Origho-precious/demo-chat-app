eeafe946a190ba3a7e93b7b275533cebb47817e3
Origho-precious <Origho9@gmail.com>
2021-09-20 15:28:05 +0100

    feat: serving html file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const express = require("express");
-const chalk = require('chalk');
+const chalk = require("chalk");
 
 const app = express();
 
@@ -8,6 +8,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, "../public")));
+
+app.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 app.listen(5000, () => {
 	console.log(chalk.bold.cyan("Server is running and listening to port 5000"));
